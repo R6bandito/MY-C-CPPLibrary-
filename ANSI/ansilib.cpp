d886247c8d97360926783efa18abd2fe205c6340
R6bandito <144506399+R6bandito@users.noreply.github.com>
2024-09-21 10:55:59 +0800

    第二次提交
    对字体颜色设置函数进行了重构，完成了光标的基本移动封装，完成了部分涉及光标处理的简单功能函数。

--- ANSI/ansilib.cpp
+++ ANSI/ansilib.cpp
@@ -1,11 +1,177 @@
 #include "ansilib.h"
 
-void __ANSI_FRONTCOLOR_SET_NOR(size_t parameter)
+#if defined(__cplusplus)
+/*文本字体的格式设置
+_text_type-----传入字体设置参数
+*/
+void __ANSI_TEXT_TYPE(size_t _text_type)
 {
-    std::cout<<"\033["<<parameter<<"m";
+    std::cout << "\033[" << _text_type << "m";
 }
 
+/*文本前景色的设置（传统模式）
+front_parameter-----字体的颜色代码
+*/
+void __ANSI_FRONTCOLOR_SET_NOR(size_t front_parameter)
+{
+    std::cout << "\033[" << front_parameter << "m";
+}
+
+/*文本的前景色设置（256色高精度设置）
+color_code-----颜色代码，范围为0-255
+*/
 void __ANSI_FRONTCOLOR_SET_ADV(size_t color_code)
 {
-    std::cout<<"\033[48;5;"<<color_code<<"m";
+    std::cout << "\033[38;5;" << color_code << "m";
 }
+
+/*文本背景色设置（传统模式）
+back_parameter-----背景色颜色代码
+*/
+void __ANSI_BACKCOLOR_SET_NOR(size_t back_parameter)
+{
+    std::cout << "\033[" << back_parameter << "m";
+}
+
+void __ANSI_BACKCOLOR_SET_ADV(size_t color_code)
+{
+    std::cout << "\033[48;5;" << color_code << "m";
+}
+
+/*光标上移设置
+lines-----移动行数，默认为1
+*/
+void __ANSI_CURSOR_UP(size_t lines)
+{
+    std::cout << "\033[" << lines << "A";
+}
+
+/*光标下移设置
+lines-----移动行数，默认为1
+*/
+void __ANSI_CURSOR_DOWN(size_t lines)
+{
+    std::cout << "\033[" << lines << "B";
+}
+
+/*光标右移设置
+columns-----列数,默认为1
+*/
+void __ANSI_CURSOR_RIGHT(size_t columns)
+{
+    std::cout << "\033[" << columns << "C";
+}
+
+/*光标左移设置
+columns-----列数，默认为1
+*/
+void __ANSI_CURSOR_LEFT(size_t columns)
+{
+    std::cout << "\033[" << columns << "D";
+}
+
+/*光标移动到指定位置
+lines_x-----行数参数
+columns_y-----列数参数
+*/
+void __ANSI_CURSOR_MOVE(size_t lines_x, size_t columns_y)
+{
+    std::cout << "\033[" << lines_x << ";" << columns_y << "H";
+}
+
+/*保存当前的光标位置
+ */
+void __ANSI_CURSOR_SAVE()
+{
+    std::cout << "\033[s";
+}
+
+/*读取保存的光标位置
+ */
+void __ANSI_CURSOR_READ()
+{
+    std::cout << "\033[u";
+}
+
+/*清屏
+ */
+void __ANSI_CLR_TERM()
+{
+    std::cout << "\033[2J\033[H";
+}
+
+/*清空光标所在行内容
+ */
+void __ANSI_LINECLR()
+{
+    std::cout << "\033[2K";
+}
+
+/*清除从光标位置到行尾的内容
+ */
+void __ANSI_LINECLR_CUR()
+{
+    std::cout << "\033[K";
+}
+
+/*隐藏光标
+ */
+void __ANSI_DISABLE_CURSOR()
+{
+    std::cout << "\033[?25l";
+}
+
+/*启用光标
+ */
+void __ANSI_ENABLE_CURSOR()
+{
+    std::cout << "\033[?25h";
+}
+
+/*禁用终端自动换行
+ */
+void __ANSI_DISABLE_AUTO_NEWLINE()
+{
+    std::cout << "\033[?7l";
+}
+
+/*启用自动换行
+ */
+void __ANSI_ENABLE_AUTO_NEWLINE()
+{
+    std::cout << "\033[?7h";
+}
+
+/*复原终端
+ */
+void resetTextAttributes();
+void __ANSI_TERM_RESET(size_t mode)
+{
+    if (mode == 1){
+        resetTextAttributes();
+        __ANSI_CLR_TERM();
+        __ANSI_ENABLE_AUTO_NEWLINE();
+    }
+    else if(mode == 0){
+        resetTextAttributes();
+    }
+}
+void resetTextAttributes()
+{
+    __ANSI_TEXT_TYPE(TEXT_TYPE_DEFAULT);
+    __ANSI_FRONTCOLOR_SET_NOR(TEXT_FRONTCOLOR_DEFAULT);
+    __ANSI_BACKCOLOR_SET_NOR(TEXT_BACKCOLOR_DEFAULT);
+    __ANSI_ENABLE_CURSOR();
+}
+
+/*该函数用于当用户进行错误输入且换行输出错误提示信息后的返回
+会清空该行的错误提示信息，并且回到光标保存位置并清空后续信息
+该函数需配合光标保存进行使用*/
+void __ANSI_INPUT_RESET()
+{
+    __ANSI_LINECLR();
+    __ANSI_CURSOR_READ();
+    __ANSI_LINECLR_CUR();
+}
+
+#endif //_cplusplus
--- ANSI/ansilib.h
+++ ANSI/ansilib.h
@@ -2,15 +2,15 @@
 #define ANSI_LIB
 
 //字体版式类型
-#define TEXT_DEFAULT    0   //重置所有效果(默认)
-#define TEXT_HEAVY      1   //字体加粗
-#define TEXT_OBSCURE    2   //字体模糊
-#define TEXT_ITALIC     3   //斜体
-#define TEXT_UNDERLIE   4   //下划线
-#define TEXT_GLINT      5   //闪烁
-#define TEXT_REVCOLOR   7   //反色
-#define TEXT_HIDE       8   //隐藏
-#define TEXT_DELLINE    9   //删除线
+#define TEXT_TYPE_DEFAULT    0   //重置所有效果(默认)
+#define TEXT_TYPE_HEAVY      1   //字体加粗
+#define TEXT_TYPE_OBSCURE    2   //字体模糊
+#define TEXT_TYPE_ITALIC     3   //斜体
+#define TEXT_TYPE_UNDERLIE   4   //下划线
+#define TEXT_TYPE_GLINT      5   //闪烁
+#define TEXT_TYPE_REVCOLOR   7   //反色
+#define TEXT_TYPE_HIDE       8   //隐藏
+#define TEXT_TYPE_DELLINE    9   //删除线
 
 //文本前景色
 #define TEXT_FRONTCOLOR_BLACK   30  //黑色
@@ -21,6 +21,7 @@
 #define TEXT_FRONTCOLOR_PRED    35  //洋红色
 #define TEXT_FRONTCOLOR_CYAN    36  //青色
 #define TEXT_FRONTCOLOR_WHITE   37  //白色
+#define TEXT_FRONTCOLOR_DEFAULT 39  //默认
 
 //文本背景色
 #define TEXT_BACKCOLOR_BLACK    40  //黑色
@@ -31,33 +32,83 @@
 #define TEXT_BACKCOLOR_PRED     45  //洋红色
 #define TEXT_BACKCOLOR_CYAN     46  //青色
 #define TEXT_BACKCOLOR_WHITE    47  //白色
+#define TEXT_BACKCOLOR_DEFAULT  49  //默认
+
+#define RESET_MODE_ALL  1   //清屏并复原整个终端设置
+#define RESET_MODE_HALF 0   //仅恢复字体设置
 
 
 #if defined(__cplusplus)
 #include <iostream>
 
-/* 终端字体修改 */
-#define __ANSI_TEXT_TYPE_HEAVY()     std::cout<<"\033["<<TEXT_HEAVY<<"m";   //字体加粗
-#define __ANSI_TEXT_TYPE_ITALIC()    std::cout<<"\033["<<TEXT_ITALIC<<"m";  //斜体字
-#define __ANSI_TEXT_TYPE_UNDERLINE()    std::cout<<"\033["<<TEXT_UNDERLIE<<"m"; //下划线
-#define __ANSI_TEXT_TYPE_GLINT()     std::cout<<"\033["<<TEXT_GLINT<<"m";   //字体闪烁
-#define __ANSI_TEXT_TYPE_REVCOLOR()  std::cout<<"\033["<<TEXT_REVCOLOR<<"m";    //字体反色
-#define __ANSI_TEXT_TYPE_HIDE()      std::cout<<"\033["<<TEXT_HIDE<<"m";    //隐藏
-#define __ANSI_TEXT_TYPE_DELLINE()   std::cout<<"\033["<<TEXT_UNDERLIE<<"m";    //字体删除线
 
-void __ANSI_FRONTCOLOR_SET_NOR(size_t parameter);   //传统文本前景色设置
+void __ANSI_TEXT_TYPE(size_t _text_type);   //文本格式设置
+
+void __ANSI_FRONTCOLOR_SET_NOR(size_t front_parameter);   //传统文本前景色设置
+
+void __ANSI_BACKCOLOR_SET_NOR(size_t back_parameter);  //传统文本背景色设置
 
 void __ANSI_FRONTCOLOR_SET_ADV(size_t color_code);   //256色文本前景色设置
 
+void __ANSI_BACKCOLOR_SET_ADV(size_t color_code);   //256色文本背景色设置
 
 
-#endif  //__cplusplus
+/*ANSI光标控制*/
 
-#if defined(_STDC_)
+void __ANSI_CURSOR_UP(size_t lines = 1);    //光标上移 
+
+void __ANSI_CURSOR_DOWN(size_t lines = 1);  //光标下移
+
+void __ANSI_CURSOR_RIGHT(size_t columns = 1);   //光标右移
+
+void __ANSI_CURSOR_LEFT(size_t columns = 1);    //光标左移
+
+void __ANSI_CURSOR_MOVE(size_t lines_x,size_t columns_y);   //光标移动到指定位置
+
+void __ANSI_CURSOR_SAVE();  //光标位置保存
+
+void __ANSI_CURSOR_READ();  //读取保存的光标位置
+
+
+void __ANSI_CLR_TERM();     //ANSI清屏
+
+void __ANSI_LINECLR();      //清空光标所在行
+
+void __ANSI_LINECLR_CUR();  //清除光标位置到行尾内容
+
+void __ANSI_DISABLE_AUTO_NEWLINE(); //禁用终端自动换行
+
+void __ANSI_ENABLE_AUTO_NEWLINE();  //启用自动换行
+
+
+
+void __ANSI_DISABLE_CURSOR();   //禁用光标
+
+void __ANSI_ENABLE_CURSOR();    //启用光标
+
+
+    #endif  //__cplusplus
+
+#if defined(__STDC__)
 #include <stdio.h>
 
 
 
-#endif  //_STDC_
+    #endif  //__STDC__
 
 #endif //ANSI_LIB
+
+
+
+#ifndef ANSI_ADVFUNC
+#define ANSI_ADVFUNC
+#if defined(__cplusplus)
+
+void __ANSI_TERM_RESET(size_t mode);   //复原终端
+
+void __ANSI_INPUT_RESET();  //输入重置
+
+    #endif //_cplusplus
+
+
+#endif //ANSI_ADVFUNC
